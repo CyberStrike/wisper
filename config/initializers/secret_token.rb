a61afed797741a328c19cb3d7b9bd8f400c6ae81
Chris Scott <Chris@Gnmlbs.com>
2014-05-04 16:34:34 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wisper::Application.config.secret_key_base = 'a273c41d130d2933bf6388ba0affeae520f54f5135b15a20f49b3416001ff495f6066c741c29a8037b0bd98f279793c523c87860dc1a23523a42236fbe2524b6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wisper::Application.config.session_store :cookie_store, key: '_wisper_session'
